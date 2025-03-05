d40bcc28800db5baa256478c29f4fa8af62bfa48
Adarsh Pandey <adarshkhushboo3@gmail.com>
2025-03-05 15:46:19 +0530

    Setting up the Environment

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+export default app;
